323848f968b07d5138bbfac80809ad2c9e7a6113
hanif-afianto <hanifsumarno@gmail.com>
2026-02-07 00:09:16 +0700

    chore: switch to official Nuxt deployment workflow

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,4 +8,7 @@ export default defineNuxtConfig({
         baseURL:
             process.env.NODE_ENV === 'production' && repo ? `/${repo}/` : '/',
     },
+    nitro: {
+        preset: 'github-pages',
+    },
 })
